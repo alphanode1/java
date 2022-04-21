f611b33cb6f4ce5f39669aa5933bc35b1de36fba
AlphaNode <prashantkr0628@gmail.com>
2022-04-21 10:24:06 +0530

    new object by declaring reference to object -> allocating an object

--- core/011_class_object/src/main/App.java
+++ core/011_class_object/src/main/App.java
@@ -5,9 +5,14 @@ class App {
 
 	   // creating an object
 	   UserProfile obj = new UserProfile();
+	   UserProfile obj2; // declare reference to object
+	   obj2 = new UserProfile(); // allocate a UserProfile object
 
 	   obj.getInput();
 	   obj.validateUser();
+
+	   obj2.getInput();
+	   obj2.validateUser();
 
    }
 }
